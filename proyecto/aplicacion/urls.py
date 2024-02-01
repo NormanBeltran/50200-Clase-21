5c7f0164b560c43023bd4050e2703382c1a81a65
NormanBeltran <josenormanbeltran@gmail.com>
2024-01-31 22:25:15 -0300

    Clase 21 - Forms, y Buscar

---
+++ proyecto/aplicacion/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('cursos/', cursos, name="cursos"),
+    path('profesores/', profesores, name="profesores"),
+    path('estudiantes/', estudiantes, name="estudiantes"),
+    path('entregables/', entregables, name="entregables"),
+    #
+    path('curso_form/', cursoForm, name="curso_form"),
+    path('prof_form/', profesorForm, name="prof_form"),
+    
+    path('buscar/', buscar, name="buscar"),
+    path('buscarCursos/', buscarCursos, name="buscarCursos"),
+]
